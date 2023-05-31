01033282d50bec9ea51840550b5db07a05b7ee9d
Luccas Vergueiro <luccas.vergueiro@confitec.com.br>
2023-05-31 17:18:12 -0300

    Primeiro commit

---
+++ Desafios-SQL/Desafio10InserindoComLogica.sql
@@ -0,0 +1,14 @@
+INSERT INTO [TABELA DE VENDEDORES]
+([NOME], 
+[MATRICULA],
+[CIDADE],
+[PERCENTUAL COMISSÃO],
+[DATA INICIO],
+[TEM DEPENDENTE])
+
+VALUES
+('João da Silva','00241','São Paulo',0.06,'2022-12-01','TRUE'),
+('Carolina Soares','00242','Rio de Janeiro',0.07,'2020-03-13','TRUE'),
+('Juliana Marques','00243','São Paulo',0.09,'2018-11-14','FALSE'),
+('Pedro Gomes','00244','São Paulo',0.08,'2019-01-20','FALSE'),
+('Christina Rodrigues','00245','Rio de Janeiro',0.07,'2021-02-13','TRUE')
---
+++ Desafios-SQL/Desafio4InserindoValores.sql
@@ -0,0 +1,4 @@
+INSERT INTO [TABELA DE VENDEDORES]
+
+VALUES('00236', 'Cláudia Morais', 0.08), 
+      ('00237','Marcela Ferreira', 0.09)
